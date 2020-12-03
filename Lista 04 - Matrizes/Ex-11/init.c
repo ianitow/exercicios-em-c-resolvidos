427551541b4d51112f4d2963cc88d2543968b18a
ianito <iaan.developer@gmail.com>
2020-12-03 00:43:58 -0300

    Ex 11 - Done (Thanks to Luiz Fernando)

--- Lista 04 - Matrizes/Ex-11/init.c
+++ Lista 04 - Matrizes/Ex-11/init.c
@@ -1,78 +1,84 @@
 #include <stdio.h>
 
-int wallyWasHere(int arr[200][200], int x, int y, int n, int m)
-{
-  int CODE_HEAD = 4;
-  int CODE_BODY = 1111;
-  int CODE_FEET = 8;
-  int CODE_HANDS = 0;
-
-  int left_hand = 0;
-  int right_hand = 0;
-  int head = 0;
-  int body = 0;
-  int feet = 0;
-  int cY = y, cX = x;
-  if (arr[y][x] == CODE_BODY)
-  {
-    body = 1;
-    if (x + 1 > n)
-    {
-      cX = -1;
-    }
-    if (arr[y][cX + 1] == CODE_HANDS)
-    {
-      right_hand = 1;
-    }
-    cX = x;
-    if (cX - 1 < 0)
-    {
-      cX = n;
-    }
-    if (arr[y][cX - 1] == CODE_HANDS)
-    {
-      left_hand = 1;
-    }
-    cY = y;
-    if (cY - 1 < 0)
-    {
-      cY = m;
-    }
-    if (arr[cY - 1][x] == CODE_HEAD)
-    {
-      head = 1;
-    }
-  }
-
-  return head && body && feet && right_hand && left_hand;
-}
 int main()
 {
-  int matriz[200][200];
-  int i, j, N, M;
-  scanf("%d %d", &N, &M);
-  if (N >= 3 && M >= 3)
+  int CODE_BODY = 1111;
+  int CODE_HEAD = 4;
+  int CODE_FEET = 8;
+  int CODE_HANDS = 0;
+  int m[200][200];
+  int i, j, largura, altura;
+  int wally = 0;
+  scanf("%d %d", &largura, &altura);
+  if (largura >= 3 && altura >= 3)
   {
-    for (i = 0; i < N; i++)
+    for (i = 0; i < largura; i++)
     {
-      for (j = 0; j < M; j++)
+      for (j = 0; j < altura; j++)
       {
-        scanf("%d", &matriz[i][j]);
+        scanf("%d", &m[i][j]);
       }
     }
-    for (i = 0; i < N; i++)
+    for (i = 0; i < largura; i++)
     {
-      for (j = 0; j < M; j++)
+      for (j = 0; j < altura; j++)
       {
-        if (wallyWasHere(matriz, j, i, N, M))
+        wally = 0;
+        if (m[i][j] == CODE_BODY)
         {
-          printf("%d %d\n", i, j);
-          return 0;
+          if (j == 0)
+          {
+            if (m[i][altura - 1] == CODE_HANDS)
+              wally++;
+            if (m[i][j + 1] == CODE_HANDS)
+              wally++;
+          }
+          else if (j == altura - 1)
+          {
+            if (m[i][0] == CODE_HANDS)
+              wally++;
+            if (m[i][j - 1] == CODE_HANDS)
+              wally++;
+          }
+          else
+          {
+            if (m[i][j + 1] == CODE_HANDS)
+              wally++;
+            if (m[i][j - 1] == CODE_HANDS)
+              wally++;
+          }
+
+          if (i == 0)
+          {
+            if (m[largura - 1][j] == CODE_HEAD)
+              wally++;
+            if (m[i + 1][j] == CODE_FEET)
+              wally++;
+          }
+          else if (i == largura - 1)
+          {
+            if (m[i - 1][j] == CODE_HEAD)
+              wally++;
+            if (m[0][j] == CODE_FEET)
+              wally++;
+          }
+          else
+          {
+            if (m[i - 1][j] == CODE_HEAD)
+              wally++;
+            if (m[i + 1][j] == CODE_FEET)
+              wally++;
+          }
+
+          if (wally == 4)
+          {
+            printf("%d %d\n", i, j);
+            return 0;
+          }
         }
       }
     }
-    printf("WALLY NAO ESTA NA MATRIZ\n");
   }
-
+  printf("WALLY NAO ESTA NA MATRIZ\n");
   return 0;
 }
