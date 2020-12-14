9c2567d843f352c6a130bdca804c2e5689454e57
ianito <iaan.developer@gmail.com>
2020-12-13 22:07:22 -0300

    Ex 07 Done

---
+++ Lista 01 - Expressão & Seleção & Repetição/02 - Seleção/Ex-07/init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  float salario, reajuste;
+  scanf("%f", &salario);
+  if (salario > 300)
+    reajuste = salario + (salario * 0.3);
+  else
+    reajuste = salario + (salario * 0.5);
+  printf("SALARIO COM REAJUSTE = %.2f\n", reajuste);
+  return 0;
+}
