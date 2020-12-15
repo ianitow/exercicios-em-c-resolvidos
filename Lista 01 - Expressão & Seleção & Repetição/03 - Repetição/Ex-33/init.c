ae228baac24837de0c8bb6f7274747e9b66b8573
ianito <iaan.developer@gmail.com>
2020-12-14 22:49:42 -0300

    Ex-33 - Done

---
+++ Lista 01 - Expressão & Seleção & Repetição/03 - Repetição/Ex-33/init.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+unsigned long long fatorial(int n)
+{
+  unsigned long long f = 1;
+
+  while (n > 0)
+    f *= n--;
+
+  return f;
+}
+int main()
+{
+  double soma = 0;
+  double valor, denominador;
+  int i, n;
+  scanf("%lf", &valor);
+  if (valor <= M_PI)
+  {
+    scanf("%d", &i);
+    for (n = 0; n <= i; n++)
+    {
+      soma += (pow((-1), n) * pow(valor, (2 * n + 1))) / fatorial((2 * n + 1));
+    }
+    printf("seno(%.2f) = %.6f\n", valor, soma);
+  }
+
+  return 0;
+}
